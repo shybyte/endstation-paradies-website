76b38b3787a7afc645ad451d3cbc76456dc945f6
shybyte <shybyte@users.noreply.github.com>
2020-04-12 19:45:42 +0200

    Removed comments

--- src/routes/musik/_songs.js
+++ src/routes/musik/_songs.js
@@ -1,25 +1,14 @@
-// Ordinarily, you'd generate this data from markdown files in your
-// repo, or fetch them from a database of some kind. But in order to
-// avoid unnecessary dependencies in the starter template, and in the
-// service of obviousness, we're just going to leave it here.
-
-// This file is called `_posts.js` rather than `posts.js`, because
-// we don't want to create an `/blog/posts` route — the leading
-// underscore tells Sapper not to do that.
-
 const songs = [
-	{
-		title: 'System',
-		slug: 'system',
-		file: '08 System'
-	},
-	{
-		title: 'Wahrheit',
-		slug: 'wahrheit',
-		file: '07 Wahrheit'
-	},
-
+  {
+    title: 'System',
+    slug: 'system',
+    file: '08 System'
+  },
+  {
+    title: 'Wahrheit',
+    slug: 'wahrheit',
+    file: '07 Wahrheit'
+  },
 ];
-
 
 export default songs;
